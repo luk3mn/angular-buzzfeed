a2677d4f16d12b3b6a52efe8e2bedb123cc2cb53
luk3mn <lucasrenan2015@gmail.com>
2024-01-11 11:11:06 -0300

    feat: creating the logic properties

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -6,7 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quizz.component.css']
 })
 export class QuizzComponent implements OnInit {
+  
+  title: string = ""
+  
+  questions: any
+  questionSelected: any
+  
+  answers: string[] = []
+  answerSelected: string = ""
 
+  questionIndex: number = 0
+  questionMaxIndex: number = 0
+
+  finished: boolean = false
+  
   constructor() { }
 
   ngOnInit(): void {
